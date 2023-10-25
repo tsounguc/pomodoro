5ae7196c9f3cbeb3c0f6270c2b6fa531ece786ee
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-25 19:17:16 -0400

    Completed User Interface

--- main.py
+++ main.py
@@ -20,11 +20,25 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+wind
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 54, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start",)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset")
+reset_button.grid(row=2, column=2)
+check_mark_text="✔"
+check_marks_label = Label(text=check_mark_text,font=(FONT_NAME, 14, "bold"), bg=YELLOW, fg=GREEN)
+check_marks_label.grid(row=3, column=1)
+
 
 window.mainloop()
