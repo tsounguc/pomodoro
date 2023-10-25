93d65c28dc013dc30b8505ccba430926a5fb4328
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-25 19:42:00 -0400

    Added count down mechanism

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -10,35 +11,46 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+def start_timer():
+    minute = 60
+    count_down(5 * minute)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-wind
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 54, "bold"), bg=YELLOW, fg=GREEN)
-timer_label.grid(row=0, column=1)
+timer_title_label = Label(text="Timer", font=(FONT_NAME, 54, "bold"), bg=YELLOW, fg=GREEN)
+timer_title_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start",)
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset")
 reset_button.grid(row=2, column=2)
-check_mark_text="✔"
-check_marks_label = Label(text=check_mark_text,font=(FONT_NAME, 14, "bold"), bg=YELLOW, fg=GREEN)
+check_mark_text = "✔"
+check_marks_label = Label(text=check_mark_text, font=(FONT_NAME, 14, "bold"), bg=YELLOW, fg=GREEN)
 check_marks_label.grid(row=3, column=1)
-
 
 window.mainloop()
